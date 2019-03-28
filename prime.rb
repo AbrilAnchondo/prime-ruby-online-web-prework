63bba3322f908ba4334758d981a40168672bc03a
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-03-28 23:37:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,6 @@
 # Add  code here!
 def prime?(number)
-   if number <= 1 
-      false 
-   elsif 
-      number!= 2 && number%2 = 0 
-      false
-   else 
-   if (2..Math.sqrt(number))%number = 0 
-      true 
-   end
- end
-end
-
-
+  
+end 
+   #if (2..Math.sqrt(number))%number = 0 
+  #    true 
