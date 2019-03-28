aec3ebab5e12b204475ef54fc0ec52ae954c7d86
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-03-28 23:54:19 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@
 def prime?(number)
   return false if number < 2 
   return true if number == 3 || number == 2
-  if (2..Math.sqrt(number)).any?{|i number%i = 0} 
+  if (2..Math.sqrt(number).any?{|i| number%i = 0} 
      true 
   else
     false
